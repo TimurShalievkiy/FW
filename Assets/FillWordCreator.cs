00e13a1d126e0d6c27eb7bbff0c8d18cd0cdef64
lKORENl <korenanon@gmail.com>
2018-10-24 09:13:02 +0300

    Check free cell and check minimum count

--- Assets/FillWordCreator.cs
+++ Assets/FillWordCreator.cs
@@ -3,38 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FillWordCreator : MonoBehaviour {
+public class FillWordCreator : MonoBehaviour
+{
 
     public GameObject CellGrid;
     public Slider NumberFirstCell;
+
+    List<List<int>> ListPassedСells;
+
     int[,] mass;
+
     public int numberLetersInFirstWord = 6;
+    int rankOfListPassedCell = 0; // номер пустой зоны
+    int lengthOfsmolestWord = 3;
+
     // Use this for initialization
-    void Start () {
-        mass = new int[5,5];
+    void Start()
+    {
+        do
+        {
+            mass = new int[5, 5];
+            ListPassedСells = new List<List<int>>();
+            rankOfListPassedCell = 0;
+            SetCellNumbers();
 
-        SetCellNumbers();
+            FillingFirstWord(mass, numberLetersInFirstWord);
+          
 
-        FillingFirstWord(mass, numberLetersInFirstWord);
-
+        } while (!CheckEmptyCells(mass, lengthOfsmolestWord));
+       
     }
-	
-	// Update is called once per frame
-	void Update () {
-        ResetFillWord();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ResetFillWord();
     }
 
     public void ChangMinCountLetters()
     {
         numberLetersInFirstWord = (int)NumberFirstCell.value;
     }
-    public void  ResetFillWord()
+    public void ResetFillWord()
     {
-        mass = new int[5, 5];
+        do
+        {
+            mass = new int[5, 5];
+            ListPassedСells = new List<List<int>>();
+            rankOfListPassedCell = 0;
+            SetCellNumbers();
 
-        SetCellNumbers();
+            FillingFirstWord(mass, numberLetersInFirstWord);
 
-        FillingFirstWord(mass, numberLetersInFirstWord);
+
+        } while (!CheckEmptyCells(mass, lengthOfsmolestWord));
     }
     void SetCellNumbers()
     {
@@ -51,7 +73,7 @@ public class FillWordCreator : MonoBehaviour {
     }
     void FillingFirstWord(int[,] mass, int numberOfLetters)
     {
-        int startCell = Random.Range(0, mass.GetLength(0) * mass.GetLength(1) );
+        int startCell = Random.Range(0, mass.GetLength(0) * mass.GetLength(1));
 
         //Debug.Log("Start cell = " + startCell);
         SetValueByNumber(1, startCell, ref mass);
@@ -66,7 +88,7 @@ public class FillWordCreator : MonoBehaviour {
                 break;
             }
             //Debug.Log("Next cell = " + x);
-           SetValueByNumber(1, x, ref mass);
+            SetValueByNumber(1, x, ref mass);
         }
     }
 
@@ -121,11 +143,11 @@ public class FillWordCreator : MonoBehaviour {
             dir[index] = right;
             index++;
         }
-       
+
         if (index == 0)
             return -1;
 
-        int index2 = Random.Range(0, index );
+        int index2 = Random.Range(0, index);
         //Debug.Log("index2 = " + index2 + " index1 = " + index);
         return dir[index2];
     }
@@ -136,4 +158,120 @@ public class FillWordCreator : MonoBehaviour {
         int j = number - i * mass.GetLength(0);
         return mass[i, j];
     }
+
+
+
+    bool CheckEmptyCells(int[,] mass, int min)
+    {
+
+        for (int i = 0; i < mass.GetLength(0); i++)
+        {
+            for (int j = 0; j < mass.GetLength(1); j++)
+            {
+                if (mass[i, j] == 0)
+                {
+                    if (!FindCellInList(mass.GetLength(0) * i + j))
+                    {
+                        ListPassedСells.Add(new List<int>());
+                        ListPassedСells[rankOfListPassedCell].Add(mass.GetLength(0) * i + j);
+                        //Debug.Log("Added in CheckEmptyCells " + (mass.GetLength(0) * i + j));
+                        CheckNearest(mass.GetLength(0) * i + j);
+                        rankOfListPassedCell++;
+                    }
+                }
+            }
+        }
+
+        string str = "";
+        foreach (var x in ListPassedСells)
+        {
+            if (x.Count < lengthOfsmolestWord)
+            {
+                Debug.Log("beeeeeeeeeeed");
+                return false;
+            }
+            else
+                Debug.Log("goooooooooood");
+            foreach (var y in x)
+            {
+
+                str += y.ToString() + " ";
+
+            }
+            str += "\n";
+        }
+        
+        Debug.Log("List count = " + ListPassedСells.Count + " \n" + str);
+        return true;
+    }
+
+    bool FindCellInList(int number)
+    {
+        if (ListPassedСells.Count == 0)
+            return false;
+
+        foreach (var x in ListPassedСells)
+        {
+            if (x.Exists(y => y == number))
+                return true;
+        }
+        return false;
+    }
+
+    void CheckNearest(int number)
+    {
+        int i = number / mass.GetLength(0);
+        int j = number - i * mass.GetLength(0);
+        //проверка вурхней ячейки на пустоту и запись
+        if (i - 1 >= 0)
+        {
+            int up = (mass.GetLength(0) * (i - 1)) + j;
+
+            if (GetValueByNubber(up) == 0 && !FindCellInList(up))
+            {
+                //Debug.Log("up = " + up + " number = " + number + " i = " + i + " j = " + j);
+                ListPassedСells[rankOfListPassedCell].Add(up);
+                CheckNearest(up);
+                //Debug.Log("Added in CheckNearest " + up);
+            }
+        }
+        //проверка нижней ячейки на пустоту и запись
+        if (i + 1 < mass.GetLength(0))
+        {
+            int down = (mass.GetLength(0) * (i + 1)) + j;
+
+            if (GetValueByNubber(down) == 0 && !FindCellInList(down))
+            {
+                //Debug.Log("down = " + down + " number = " + number + " i = " + i + " j = " + j);
+                ListPassedСells[rankOfListPassedCell].Add(down);
+                CheckNearest(down);
+                //Debug.Log("Added in CheckNearest " + down);
+            }
+        }
+        //проверка левой ячейки на пустоту и запись
+        if (j - 1 >= 0)
+        {
+            int left = (mass.GetLength(0) * i) + j - 1;
+            // Debug.Log("ltft = " + left + " number = " + number + " i = " + i + " j = " + j);
+            if (GetValueByNubber(left) == 0 && !FindCellInList(left))
+            {
+                ListPassedСells[rankOfListPassedCell].Add(left);
+                CheckNearest(left);
+                //Debug.Log("Added in CheckNearest " + left);
+            }
+        }
+        //проверка правой ячейки на пустоту и запись
+        if (j + 1 < mass.GetLength(1))
+        {
+            int right = (mass.GetLength(0) * i) + j + 1;
+            // Debug.Log("right = " + right + " number = " + number + " i = " + i + " j = " + j);
+            if (GetValueByNubber(right) == 0 && !FindCellInList(right))
+            {
+                ListPassedСells[rankOfListPassedCell].Add(right);
+                CheckNearest(right);
+                //Debug.Log("Added in CheckNearest " + right);
+            }
+        }
+    }
+
 }
